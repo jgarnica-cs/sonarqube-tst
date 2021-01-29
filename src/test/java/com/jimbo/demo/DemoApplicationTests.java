67bbfc0c46c3a28732758fb9193b5920432e6da4
Juan Carlos Garnica <jgarnica@creditshop.com>
2021-01-29 12:07:40 -0600

    First commit

---
+++ src/test/java/com/jimbo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimbo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
