0aa4d6a23295b43eba38d866a26604b68fb5e12f
Juan Carlos Garnica <jgarnica@creditshop.com>
2021-02-01 12:56:05 -0600

    Test branch

--- src/test/java/com/jimbo/demo/service/TestServiceTest.java
+++ src/test/java/com/jimbo/demo/service/TestServiceTest.java
@@ -1,9 +1,18 @@
 package com.jimbo.demo.service;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestServiceTest
 {
+    @Mock
+    private TestServiceImpl testService;
+
+    @Test
+    public void test() {
+        testService.testFunction();
+    }
 }
