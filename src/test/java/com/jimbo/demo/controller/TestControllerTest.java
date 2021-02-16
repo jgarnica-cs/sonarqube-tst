1094277ff98e297283e8920cf253d396b559fab3
Juan Carlos Garnica <jgarnica@creditshop.com>
2021-02-16 11:09:56 -0600

    POM

--- src/test/java/com/jimbo/demo/controller/TestControllerTest.java
+++ src/test/java/com/jimbo/demo/controller/TestControllerTest.java
@@ -1,5 +1,25 @@
 package com.jimbo.demo.controller;
 
+import com.jimbo.demo.service.TestServiceImpl;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
 public class TestControllerTest
 {
+    @Mock
+    private TestServiceImpl testService;
+
+    @InjectMocks
+    private TestController testController;
+
+    @Test
+    public void test() {
+        testController.call();
+    }
+
+    @Test
+    public void test2() {
+        testController.newTest();
+    }
 }
